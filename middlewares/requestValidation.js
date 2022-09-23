4e9e7f21b4feb652e6e464874761087dd90d6a67
Kostya Sterkhov <sterxovk@mail.ru>
2022-09-24 02:22:59 +0400

    fix validDelete

--- middlewares/requestValidation.js
+++ middlewares/requestValidation.js
@@ -44,8 +44,7 @@ const validateCreateMovie = celebrate({
 
 const validateDeleteMovie = celebrate({
   params: Joi.object().keys({
-    movieId: Joi.string().required().alphanum().length(24)
-      .hex(),
+    movieId: Joi.string().required().number(),
   }),
 });
 
